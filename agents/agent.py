bbb4ccc8e078c5c0a2ceebf5549afe5609990f3a
bababuck <buchner.ryan@gmail.com>
2023-05-11 14:57:27 -0700

    Implement generate_sample() in Agent

--- agents/agent.py
+++ agents/agent.py
@@ -27,3 +27,31 @@ class Agent():
         """
         entropy = forward(states) * log(forward(states))
         minimize(rewards - entropy)
+
+    def generate_samples(self, env, max_states, max_states_per_trajectory):
+        """ Generate a set of sample trajectories from the enviroment.
+
+        Inputs:
+        env - ControlEnv type
+        max_states - number of total states to visit
+        max_states_per_trajectory - max timesteps for a single rollout
+
+        Outputs:
+        states - a list of all states visited
+        actions - a list of the action at each state visited
+        """
+        states, actions = [], []
+        states_visited = 0
+        while states_visited < max_states:
+            state = env.reset()
+            for _ in range(max_t):
+                states_visited += 1
+                action = self.get_action(state, false)
+                states.append(states)
+                actions.append(action)
+
+                state, reward, done = env.step(action)
+                if done:
+                    break
+
+        return states, actions
