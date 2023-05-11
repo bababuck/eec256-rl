22c18872cc051f3c45a0d43a31dbebfb2b129666
bababuck <buchner.ryan@gmail.com>
2023-05-11 14:07:48 -0700

    Add pseudocode for training loop for trainer

--- agents/trainer.py
+++ agents/trainer.py
@@ -1,4 +1,6 @@
 from env.control import Env
+from agent import Agent
+from cost import Cost
 
 class Trainer():
     """ Class for training the RL agent. """
@@ -7,6 +9,7 @@ class Trainer():
         """ Create the gym enviroment and agent. """
         self.env = Env()
         self.agent = Agent()
+        self.cost = Cost()
 
     def training_loop(self, rollouts):
         """ Train the agent.
@@ -18,4 +21,23 @@ class Trainer():
             self.simulate()
             
     def simulate():
-        """ Run one rollout. ""
+        """ Run one rollout. """
+
+    def training_loop():
+        """ Main training loop per GCL.
+
+        Algorithm 1 from the paper.
+        """
+        # Initialize q_k(τ) as either a random initial controller or from demonstrations
+        d_demo = Batch("demo_path.npy")
+        # for iteration i = 1 to I:
+        for i in range(iterations):
+            # Generate samples Dtraj from qk(τ )
+            d_traj = self.agent.generate_samples(self.env)
+            # Append samples: Dsamp ← Dsamp ∪ Dtraj
+            d_samp = d_samp + d_traj
+            # Use Dsamp to update cost cθ using Algorithm 2
+            self.cost.non_linear_ioc(d_demo, d_samp)
+            # Update qk(τ ) using Dtraj and the method from (Levine & Abbeel, 2014) to obtain qk+1(τ )
+            self.agent.update()
+        # return optimized cost parameters θ and trajectory distribution q(τ )
