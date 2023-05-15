30029cec708ae54747c8a9673703fa321d3889de
bababuck <buchner.ryan@gmail.com>
2023-05-14 23:18:35 -0700

    Begin to implement ControlEnv class
    Should be able to start with training once this is finished

--- env/control.py
+++ env/control.py
@@ -1,7 +1,18 @@
+from agent.random_agent import RandomAgent
+import gymnasium as gym
+
 class ControlEnv:
     """ Interface for our RL agent to interact with the enviroment. """
 
+    NUM_RND_ACTIONS - 50 # Number of random actions upon state reset
+
     def __init__():
+        """
+
+        Random agent will be used to randomly initialize the state.
+        """
+        self.random_agent = RandomAgent(num_actions)
+        self.env = gym.make("simple_rope", render_mode="human")
 
     def step(self, action):
         """ Take one action in the simulation.
@@ -14,6 +25,7 @@ class ControlEnv:
         reward - reward from the prior action
         done - is episode complete
         """
+        observation, reward, done, truncated, info = self.env.step(action)
         return observation, reward, done
 
     def reset(self):
@@ -22,3 +34,7 @@ class ControlEnv:
         Outputs:
         observation - state of enviroment following the reset
         """
+        observation, info = self.env.reset()
+        for i in range(NUM_RND_ACTIONS):
+            self.
+        return observation
