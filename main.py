393b5575baafb706a7e52994bdefd54920705db8
bababuck <buchner.ryan@gmail.com>
2023-05-25 12:00:54 -0700

    Muck up main.py for testing

--- main.py
+++ main.py
@@ -2,12 +2,75 @@ from agents.trainer import Trainer
 from agents.agent import Agent
 from agents.cost import Cost
 from env.control import ControlEnv
+import time
+import gym_roborope
+import gymnasium as gym
+import numpy as np
 
 if __name__ == '__main__':
-    env = ControlEnv('CartPole-v1')
+#    env = ControlEnv('CartPole-v1')
+#    env = ControlEnv('CartPole-v1')
+#    gym.register(
+#    id="roborope-v0",
+#    entry_point=RopeEnv,
+#    )
+    env = gym.make('FetchPickAndPlace-v2', max_episode_steps=100, render_mode = "human")
+#    env = gym.make('RoboRope-v0', render_mode = "human")
     env.reset()
     env.render()
+    time.sleep(2)
+    action = np.array([0, 1, 0, 0])
+    env.step(action)
+    time.sleep(1)
+    action = np.array([1, 0, 0, 0])
+    env.step(action)
+    time.sleep(1)
+#    action = np.array([0, 0, 0, -0.02])
+#    env.step(action)
+    time.sleep(1)
+    action = np.array([0, 0, -1, 0])
+    env.step(action)
+    action = np.array([0, 0, -1, 0])
+    env.step(action)
+    action = np.array([0, 0, -1, 0])
+    env.step(action)
+    action = np.array([0, 0, -1, 0])
+    env.step(action)
+    action = np.array([0, 0, -1, 0])
+    env.step(action)
+    action = np.array([0, 0, -1, 0])
+    env.step(action)
+    time.sleep(1)
+    action = np.array([0, 0, 0, 1])
+    env.step(action)
+    time.sleep(1)
+    action = np.array([0, 0, 0, -1])
+    env.step(action)
+    time.sleep(1)
+    action = np.array([0, 0, 0, -1])
+    env.step(action)
+    time.sleep(1)
+    action = np.array([0, 0, 0, -0.02])
+    env.step(action)
+    time.sleep(1)
+    action = np.array([0, 0, 0, -0.02])
+    env.step(action)
+    action = np.array([0, -1, 0, 0])
+    env.step(action)
+    time.sleep(1)
+    action = np.array([0, -1, 0, 0])
+    env.step(action)
+    time.sleep(1)
+    action = np.array([0, -1, 0, 0])
+    env.step(action)
+    time.sleep(1)
+    action = np.array([0, -1, 0, 0])
+    env.step(action)
+    time.sleep(1)
+    time.sleep(50)
     env.close()
+#    env.end_render()
+"""
     action_size = env.action_space.n
     state_size = env.observation_space.shape[0]
     hidden_layer_size = 32
@@ -20,4 +83,4 @@ if __name__ == '__main__':
 
     networks_folder = "netowrks"
     trainer.save_networks(networks_folder)
-    agent.generate_samples(env)
+    agent.generate_samples(env)"""
