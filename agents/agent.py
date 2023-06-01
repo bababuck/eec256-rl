164fb9ef12406ab35fdd2ee001df067943ef77e6
fy-meng <fanyu.meng98@gmail.com>
2023-06-01 03:40:13 -0700

    [feat] finish agent who output both type of action

--- agents/agent.py
+++ agents/agent.py
@@ -1,77 +1,178 @@
 from utils.batch import Batch
-import utils.utils as utils
 import torch
+import torch.nn as nn
 import numpy as np
 
-class Agent():
+
+class Agent:
     """ Agent class that gives actions based on current state. """
 
-    def __init__(self, action_size, state_size, hidden_layer_size, hidden_layers):
-        """ Init network and optimizer. """
-        self.net = utils.generate_simple_network(state_size, action_size, hidden_layer_size, hidden_layers)
-        self.optimizer = torch.optim.Adam(self.net.parameters(), lr=0.001)
-
-    def get_random_action(self, state):
-        """ Return an action based on the current state randomly chosed from model distribution.
-
-        Inputs:
-        state - observable state
-
-        Outputs:
-        action - action to perform
-        probs - probability of each_action
+    def __init__(self, discrete_action_size, cont_action_size, cont_action_min, cont_action_max,
+                 state_size, hidden_layers, lr=0.001):
         """
-        probs = self.get_probs(state, False)
-        action = np.random.choice(2, p = probs.numpy())
-        return action, probs
+        Init network and optimizer.
+        Parameters
+        ----------
+        discrete_action_size : int
+            Dimension of the continuous part of the action space.
+        cont_action_size : int
+            Dimension of the continuous part of the action space.
+        cont_action_min, cont_action_max : 1-D array-like
+            The bound of the continuous part of the action. Size should be (cont_action_size,).
+            Used to generate the random actions.
+        state_size : int
+            Dimension of the state space.
+        hidden_layers : 1-D array-like
+            The size of each hidden layer.
+        lr : float
+            The learning rate.
+        """
+        self.discrete_action_size = discrete_action_size
+        self.cont_action_size = cont_action_size
+        self.cont_action_min = cont_action_min
+        self.cont_action_max = cont_action_max
+
+        # Continuous action based on NAF in https://arxiv.org/pdf/1603.00748.pdf
+        # For each discrete action, the output is a lower triangular matrix L of size m
+        # and a vector μ of size m. There is also an additional real value.
+        n = discrete_action_size
+        m = cont_action_size
+        n_outputs_per_bin = (m + 1) * m // 2 + m + 1
+        n_outputs = n * n_outputs_per_bin
+
+        layers = []
+        prev_size = state_size
+        for size in hidden_layers:
+            layers += [nn.Linear(prev_size, size), nn.ReLU(inplace=True)]
+            prev_size = size
+        layers.append(nn.Linear(prev_size, n_outputs))
+        self.net = nn.Sequential(*layers)
+        self.optimizer = torch.optim.Adam(self.net.parameters(), lr=lr)
+
+    def get_random_action(self, _state):
+        """
+        Return a random action.
+        The discrete action is chosen uniformly, and the continuous action is sampled
+        uniformly between `cont_action_min` and `cont_action_max`.
+        Parameters
+        ----------
+        _state : 1-D array-like
+            The observed state. ignored.
+
+        Returns
+        -------
+        action : 1-D ndarray
+            The random action. The first dimension is the discrete action index, and the
+            rest are continuous action
+        """
+        discrete_action_idx = np.random.randint(self.discrete_action_size)
+        cont_action = np.random.uniform(self.cont_action_min, self.cont_action_max)
+        action = np.concatenate(([discrete_action_idx], cont_action))
+        return action
+
+    def reformat_output(self, output):
+        """
+        Reformat the network output. Gradients are kept.
+        Parameters
+        ----------
+        output: 1-D Tensor
+            Network output. Size (n * ((m + 1) * m // 2 + m + 1),)
+        Returns
+        -------
+        Ls: 3-D Tensor
+            Lower triangular matrices. Size (n, m, m). Each (m, m) matrix is lower triangular.
+        mus: 2-D Tensor
+            Action mean. Size (n, m).
+        qs: 1-D Tensor
+            Q-value estimator for each discrete action. Size (n,).
+        """
+        n = self.discrete_action_size
+        m = self.cont_action_size
+        n_triangular_entries = (m + 1) * m // 2
+
+        Ls = torch.zeros((n, m, m))
+        tril_indices = torch.tril_indices(m, m)
+        Ls[:, tril_indices[0], tril_indices[1]] = torch.reshape(output[:n * n_triangular_entries],
+                                                                (n, n_triangular_entries))
+        mus = torch.reshape(output[n * n_triangular_entries: n * (n_triangular_entries + m)],
+                            (n, m))
+        qs = output[n * (n_triangular_entries + m):]
+        assert qs.shape == (n,)
+        return Ls, mus, qs
 
     def get_policy_action(self, state):
-        """ Return an action based on the current state chosen as most probable action from distribution.
-
-        Inputs:
-        state - observable state
-
-        Outputs:
-        action - action to perform
-        probs - probability of each_action
         """
-        probs = self.get_probs(state, False)
-        action = np.argmax(probs.numpy())
-        return action, probs
+        Return the best action based on the input state based on the agent's estimation.
+        Parameters
+        ----------
+        state : 1-D array-like
+        The observed state.
 
-    def get_probs(self, state, training):
-        """ Return probability distribution based on current state.
-
-        Inputs:
-        state - observable state
-        training - boolean if training or evaluating
-
-        Outputs:
-        action - action to perform
-        probs - probability of each_action
+        Returns
+        -------
+        action : 1-D ndarray
+            The policy's best action. The first dimension is the discrete action index,
+            and the rest are continuous action.
         """
-        logits = self.net.forward(state)
-        probs = torch.softmax(logits,-1)
-        if not training:
-            probs = probs.detach()
-        return probs
+        if not isinstance(state, torch.Tensor):
+            state = torch.Tensor(state)
+        output = self.net(state)
+        _, mus, qs = self.reformat_output(output)
+        mus = mus.detach().numpy()
+        qs = qs.detach().numpy()
 
-    def update(self, states, cost, probs):
-        """ Update policy and value functions based on a set of observations.
+        discrete_action_idx = np.argmax(qs)
+        cont_action = mus[discrete_action_idx]
+        action = np.concatenate(([discrete_action_idx], cont_action))
+        return action
 
-        minq Eq[cθ(τ)] − H(τ)
-
-        Inputs:
-        states - sequence of observed states
-        rewards - sequence of rewards from the performed actions
+    def get_q_values(self, state, discrete_action, cont_action):
         """
-        probs = self.get_probs(states[:,:4], True)
-        log_probs = torch.log(probs+1e-7)
-        cost = torch.tensor(cost,dtype=torch.float32)
-        cost = torch.mean(probs * cost, dim=-1)
-        entropy = torch.mean(torch.mul(probs, log_probs), dim=-1)
-        loss = torch.mean(cost - entropy*1e-3)
+        Return the Q-value for each discrete action.
+        Parameters
+        ----------
+        state : 1-D array-like
+            The observed state.
+        discrete_action : int
+            The discrete part of the action.
+        cont_action : 1-D array-like
+            The continuous part of the action.
+        Returns
+        -------
+        """
+        if not isinstance(state, torch.Tensor):
+            state = torch.Tensor(state)
+        if not isinstance(cont_action, torch.Tensor):
+            cont_action = torch.Tensor(cont_action)
 
+        output = self.net(state)
+        Ls, mus, qs = self.reformat_output(output)
+
+        L = Ls[discrete_action]
+        mu = mus[discrete_action]
+        q = qs[discrete_action]
+
+        return q - 0.5 * (cont_action - mu) @ L @ L.T @ (cont_action - mu)
+
+    def update(self, state, action, target):
+        """
+        Update the Q-value estimation from on one step.
+        Parameters
+        ----------
+        state : 1-D array-like
+            The observed state.
+        action : 1-D array-like
+            The discrete part of the action.
+        action : 1-D ndarray
+            The action. The first dimension is the discrete action index,
+            and the rest are continuous action.
+        target : float
+            Target value.
+        """
+        discrete_action = action[0]
+        cont_action = action[1:]
+        q = self.get_q_values(state, discrete_action, cont_action)
+        loss = nn.MSELoss(target, q)
         self.optimizer.zero_grad()
         loss.backward()
         self.optimizer.step()
@@ -81,18 +182,19 @@ class Agent():
 
         Supervised training will not
         """
-        states = torch.tensor(states, dtype=torch.float32)
-        truth = torch.tensor(truth, dtype=torch.float32)
-        mse_loss = torch.nn.MSELoss()
-        probs = self.get_probs(states, True)
-        loss = mse_loss(probs, truth)
-
-        self.optimizer.zero_grad()
-        loss.backward()
-        self.optimizer.step()
+        raise NotImplemented
+        # states = torch.tensor(states, dtype=torch.float32)
+        # truth = torch.tensor(truth, dtype=torch.float32)
+        # mse_loss = torch.nn.MSELoss()
+        # probs = self.get_probs(states, True)
+        # loss = mse_loss(probs, truth)
+        #
+        # self.optimizer.zero_grad()
+        # loss.backward()
+        # self.optimizer.step()
 
     def generate_samples(self, env, max_states, max_states_per_trajectory):
-        """ Generate a set of sample trajectories from the enviroment.
+        """ Generate a set of sample trajectories from the environment.
 
         Inputs:
         env - ControlEnv type
@@ -102,24 +204,25 @@ class Agent():
         Outputs:
         batch - batch containing the rewards, states, and actions
         """
-        states, actions, probs = [], [], []
-        states_visited = 0
-        while states_visited < max_states:
-            state = env.reset()
-            action, prob = self.get_random_action(torch.tensor(state))
-            for i in range(max_states_per_trajectory):
-                states.append(state)
-                actions.append(action)
-                probs.append(prob.numpy()[action])
-
-                states_visited += 1
-                state, reward, done = env.step(action)
-                action, prob = self.get_random_action(torch.tensor(state))
-                    
-                if done:
-                    break
-
-        return Batch(states=states, actions=actions, probs=probs)
+        raise NotImplemented
+        # states, actions, probs = [], [], []
+        # states_visited = 0
+        # while states_visited < max_states:
+        #     state = env.reset()
+        #     action, prob = self.get_random_action(torch.tensor(state))
+        #     for i in range(max_states_per_trajectory):
+        #         states.append(state)
+        #         actions.append(action)
+        #         probs.append(prob.numpy()[action])
+        #
+        #         states_visited += 1
+        #         state, reward, done = env.step(action)
+        #         action, prob = self.get_random_action(torch.tensor(state))
+        #
+        #         if done:
+        #             break
+        #
+        # return Batch(states=states, actions=actions, probs=probs)
 
     def test(self, env, num_test):
         """ Run a test of the model on the enviroment.
