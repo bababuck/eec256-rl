fac7a2ff83f022f87424d30513fceb3b8c58d978
bababuck <buchner.ryan@gmail.com>
2023-06-11 18:33:49 -0700

    Clean up

--- easy_to_max/env/control.py
+++ easy_to_max/env/control.py
@@ -11,7 +11,7 @@ import torch
 class ControlEnv():
     """ Interface for our RL agent to interact with the enviroment. """
 
-    NUM_RND_ACTIONS = 3 # Number of random actions upon state reset
+    ACTION_THRESHOLD=0.001 # How accurate do we want our actions to be
 
     def __init__(self, add_randomness=False, sleep_time=0):
         """
@@ -22,26 +22,15 @@ class ControlEnv():
         if add_randomness:
             self.random_agent = True
         else:
-            self.random_agent = None
+            self.random_agent = False
         np.random.seed(0)
         self.sleep_time = sleep_time
 
-    @property
-    def action_space(self):
-        return 4
-
-    @property
-    def observation_space(self):
-        return 24
-
-    @property
-    def rope_observation_space(self):
-        return 12
-
-    def step(self, segment, direction):
+    def step(self, segment, action, reset=False):
         """ Take one action in the simulation.
 
         Inputs:
+        seg    - segment to grab
         action - action for the agent to take
                - encoded as [segment, dx, dy]
 
@@ -50,28 +39,25 @@ class ControlEnv():
         reward - reward from the prior action
         done - is episode complete
         """
-        observation, reward, done = self.do_actions(segment, direction)
-        return observation, reward, done
-
-    def do_actions(self, seg, action, reset=False):
+        if reset:
+            # 8 segment options in underlying environment, but 4 options provided by the wrapper
+            seg = {0:0, 1:2, 2:4, 3:7}[segment]
+        else:
+            seg = {0:0, 1:7}[segment] # One hot
 
         # First move to picked location
-        if seg == 1:  # One hot
-            seg = 7
-        elif seg == 2:
-            seg = 4
-        elif seg == 3:
-            seg = 7
         new_x, new_y, _ = self.env.get_rope_pos(seg)
         curr_x, curr_y, _ = self.env.get_gripper_xpos()
-        while (curr_x > new_x + 0.001) or (curr_x < new_x - 0.001) or (curr_y > new_y + 0.001) or (curr_y < new_y - 0.001):
+        while (curr_x > new_x + self.ACTION_THRESHOLD) or (curr_x < new_x - self.ACTION_THRESHOLD) or (curr_y > new_y + self.ACTION_THRESHOLD) or (curr_y < new_y - self.ACTION_THRESHOLD):
             curr_x, curr_y, _ = self.env.get_gripper_xpos()
             move_x = min(1, (new_x - curr_x)*20)
             move_y = min(1, (new_y - curr_y)*20)
             self.env.step(np.array([move_x, move_y, 0, 0]))
-        # Then lower
+
+        # Then lower the gripper
         self.env.step(np.array([0, 0, -0.75, 0]))
-        # Then move
+
+        # Then move the speicifed distance
         dx = action[0]
         dy = action[1]
         dx = dx/20
@@ -82,38 +68,48 @@ class ControlEnv():
                 self.render()
                 time.sleep(self.sleep_time)
 
-        # Then raise
+        # Then raise the gripper
         self.reset_gripper_height()
-        self.count += 1
+
+        if not reset:
+            self.count += 1
         state = self.get_rope_states() 
 
+        # Rollout is done if:
+        # - more steps than MAX_EPISODE_STEPS
+        # - any segment is no longer reachable by the gripper
+        #      - This range is roughly empirically determined   
         done = self.count > 50
-        for i in range(1,12,2):#[1,3,9,11]:#range(1,12,2):
+        for i in range(1,12,2):
             if state[i] < 0.4 or state[i] > 1.1:
                 done = True
-        for i in range(0,12,2):#[0,2,8,10]:#range(0,12,2):
-            if state[i] < 1.05  or state[i] > 1.47:
+        for i in range(0,12,2):
+            if state[i] < 1.13  or state[i] > 1.47:
                 done = True
 
         normalize_states(state)
 
-        return state, 0, done
+        # Never give reward
+        reward = 0
+
+        return state, reward, done
 
     def get_rope_states(self):
+        """ Get the state of all the ropes. """
         state = []
         for i in [0, 2, 3, 4, 5, 7]:
             x, y, _ = self.env.get_rope_pos(i)
             state.append(x)
             state.append(y)
-        state = state
-#        for i in [4,5,6,7]:
-#            state[i] = 0
         return np.array(state)
 
     def reset_gripper_height(self):
+        """ Move the gripper to a set height above the rope.
+        Can move freely in the x-y plane at this height.
+        """
         _, _, curr_z = self.env.get_gripper_xpos()
         new_z = 0.475
-        while (curr_z > new_z + 0.001) or (curr_z < new_z - 0.001):
+        while (curr_z > new_z + self.ACTION_THRESHOLD) or (curr_z < new_z - self.ACTION_THRESHOLD):
             _, _, curr_z = self.env.get_gripper_xpos()
             move_z = min(1, (new_z - curr_z)*20)
             self.env.step(np.array([0, 0, move_z, 0]))
@@ -146,7 +142,7 @@ class ControlEnv():
               [2, 2, 2, 2, 0, 2, 1, 2, 3, 1, 3, 1, 0, 0, 0, 0, 0, 0, 2, 0, 3, 1, 0, 0, 3, 1],
               [0, 2, 0, 2, 1, 3, 3, 1, 1, 3, 0, 0, 2, 0, 2, 1, 0, 0, 0, 0]]
 
-        if self.random_agent is not None:
+        if self.random_agent:
             setup = setups[np.random.randint(0,15)]
             for i in range(0, np.random.randint(0, len(setup)), 2):
                 directions = [0, 0]
@@ -158,7 +154,7 @@ class ControlEnv():
                     directions = [0, 1]
                 elif setup[i + 1] == 3:
                     directions = [0, -1]
-                self.do_actions(setup[i], directions, True)
+                self.step(setup[i], directions, True)
 
         state = self.get_rope_states()
         normalize_states(state)
--- easy_to_max/expert_data/Makefile
+++
@@ -1,5 +0,0 @@
-run:
-	python3 main.py
-
-#generate_demos:
-#	python3 demo.py
